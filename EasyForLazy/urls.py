a5972764fe2795644e51d689dd26b2ff103c4063
vnitikesh <vnitikesh@gmail.com>
2020-11-30 20:02:14 +0530

    Initial Commit

---
+++ EasyForLazy/urls.py
@@ -0,0 +1,33 @@
+"""EasyForLazy URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from authorize import views
+from rest_framework import routers
+from knox import views as knox_views
+
+router = routers.DefaultRouter()
+router.register(r'users',views.UserViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/register/',views.RegisterAPI.as_view(),name = 'register'),
+    path('',include(router.urls)),
+    path('api/login/', views.LoginAPI.as_view(), name = 'login'),
+    path('api/logout/', knox_views.LogoutView.as_view(),name = 'logout'),
+    path('api/logoutall/',knox_views.LogoutAllView.as_view(), name = 'logoutall'),
+    path('api/change-password/', views.ChangePasswordView.as_view(), name = 'change-password'),
+]
---
+++ authorize/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from rest_framework import generics, permissions, viewsets, permissions, status
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import UserSerializer, RegisterSerializer, ChangePasswordSerializer
+from django.contrib.auth.models import User
+from knox.views import LoginView as KnoxLoginView
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from django.contrib.auth import login
+
+#Regsiter API
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        user = serializer.save()
+        return Response({
+        "user": UserSerializer(user, context = self.get_serializer_context()).data,
+        "token": AuthToken.objects.create(user)[1]
+        })
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny, )
+
+    def post(self, request, *args, **kwargs):
+        serializer = AuthTokenSerializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format = None)
+
+class ChangePasswordView(generics.UpdateAPIView):
+    serializer_class = ChangePasswordSerializer
+    model = User
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def get_object(self, queryset = None):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data = request.data)
+
+        if(serializer.is_valid()):
+            if(not self.object.check_password(serializer.data.get("old_password"))):
+                return Response({"old_password": ["Wrong password"]}, status = status.HTTP_400_BAD_REQEUST)
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            response = {
+                'status': 'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Password Updated Successfully',
+                'data': []
+            }
+            return Response(response)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQEUST)
